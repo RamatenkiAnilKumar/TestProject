8321387edc021dcb3f636f358eef9a598fab12f5
RamatenkiAnilKumar <46487522+RamatenkiAnilKumar@users.noreply.github.com>
2019-02-09 18:03:46 +0530

    Update Test.java
    addition two numbers added

--- TestProject/src/Test.java
+++ TestProject/src/Test.java
@@ -1,8 +1,11 @@
 
 public class Test {
 	public static void main(String args[])
+		int a=10,b=30;
+	int c= a+b;
 	{
 		System.out.println("Hello There!");
+		System.out.println("Hello Addition!"+c);
 	}
 
 }
