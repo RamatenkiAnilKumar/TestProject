7ff34bfd050849a2f76df12fa40e926bb4cc9e87
RamatenkiAnilKumar <46487522+RamatenkiAnilKumar@users.noreply.github.com>
2019-02-09 18:07:02 +0530

    data not modified
    simple Hello world

--- TestProject/src/Test.java
+++ TestProject/src/Test.java
@@ -1,11 +1,9 @@
 
 public class Test {
 	public static void main(String args[])
-		int a=10,b=30;
-	int c= a+b;
+		
 	{
 		System.out.println("Hello There!");
-		System.out.println("Hello Addition!"+c);
 	}
 
 }
